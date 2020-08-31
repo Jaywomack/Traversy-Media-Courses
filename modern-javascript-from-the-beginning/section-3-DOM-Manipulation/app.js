4e88e16fc990556be36107e438c8ae206bd4c905
jaywomack <womackjw@hotmail.com>
2020-08-31 12:25:47 -0700

    Moving on to traversing the DOM

--- modern-javascript-from-the-beginning/section-3-DOM-Manipulation/app.js
+++ modern-javascript-from-the-beginning/section-3-DOM-Manipulation/app.js
@@ -90,13 +90,55 @@
  */
 
 // document.getElementsByClassName
-const items = document.getElementsByClassName("collection-item");
+// const items = document.getElementsByClassName("collection-item");
+
+// console.log(items);
+// console.log(items[0]);
+// items[0].style.color = "red";
+// items[3].textContent = "Hello";
+
+// const listItems = document
+//   .querySelector("ul")
+//   .getElementsByClassName("collection-item");
+
+// console.log(listItems);
+
+// Document.getElementByTagName
+// let lis = document.getElementsByClassName("collection-item");
+
+// console.log(lis);
+// console.log(lis[0]);
+// lis[0].style.color = "red";
+// lis[1].textContent = "Hello";
+// // Convert HTML Collection into an array
+// lis = Array.from(lis);
+
+// lis.reverse();
+
+// lis.forEach((li, index) => {
+//   console.log(li.className);
+//   li.textContent = `${index}Hello`;
+// });
+
+// console.log(lis);
+// Query Selector All
+// Returns a node list
+
+// document.querySelectorAll()
+
+const items = document.querySelectorAll("ul.collection li.collection-item");
+
+items.forEach((item, index) => {
+  item.textContent = `${index}: task`;
+});
+
+const liOdd = document.querySelectorAll("li:nth-child(odd)");
+const liEven = document.querySelectorAll("li:nth-child(even)");
+liOdd.forEach((li) => {
+  li.style.background = "#ccc";
+});
+liEven.forEach((li) => {
+  li.style.background = "#f4f4f4";
+});
 
 console.log(items);
-console.log(items[0]);
-items[0].style.color = "red";
-items[3].textContent = "Hello";
-
-const listItems = document
-  .querySelector("ul")
-  .getElementsByClassName("collection-item");
