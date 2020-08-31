e1d09fc6e48a88c1eda2d8d37197a6fbeb8c3916
jaywomack <womackjw@hotmail.com>
2020-08-31 16:58:54 -0700

    finished event listeners and event

--- modern-javascript-from-the-beginning/section-3-DOM-Manipulation/app.js
+++ modern-javascript-from-the-beginning/section-3-DOM-Manipulation/app.js
@@ -208,3 +208,123 @@
  *
  * CREATING ELEMENTS
  */
+
+// const li = document.createElement("li");
+
+// // Add class
+// li.className = "collection-item";
+
+// // Add ID
+// li.id = "new-item";
+
+// // Add attribute
+// li.setAttribute("title", "New Item");
+
+// // Create text node and append
+// li.appendChild(document.createTextNode("Hello World"));
+
+// // Create new link element
+// const link = document.createElement("a");
+// // Add classes
+// link.className = "delete-item secondary-content";
+// // Add icon html
+// link.innerHTML = "<i class='fa fa-remove'></i>";
+
+// // Append link into li
+// li.appendChild(link);
+
+// // Append li as child to ul
+// document.querySelector("ul.collection").appendChild(li);
+
+// console.log(li);
+
+/***
+ *
+ * REMOVING AND REPLACING ELEMENTS
+ */
+
+// // Create and element
+// const newHeading = document.createElement("h2");
+// // Add id
+// newHeading.id = "task-title";
+// // New Text node
+// newHeading.appendChild(document.createTextNode("Task List"));
+
+// // Get the old heading
+// const oldHeading = document.getElementById("task-title");
+// // Parent
+// const cardAction = document.querySelector(".card-action");
+
+// // Replace
+// cardAction.replaceChild(newHeading, oldHeading);
+
+// // REMOVE ELEMENT
+// const lis = document.querySelectorAll("li");
+// const list = document.querySelector("ul");
+
+// // Remove list item
+// lis[0].remove();
+
+// // Remove child element
+// list.removeChild(lis[3]);
+
+// // CLASSES AND ATTRIBUTES
+// const firstLi = document.querySelector("li:first-child");
+// const link = firstLi.children[0];
+
+// let val;
+// // Classes
+// val = link.className;
+// val = link.classList;
+// val = link.classList[0];
+// link.classList.add("test");
+// link.classList.remove("test");
+// val = link;
+
+// // Attributes
+// val = link.getAttribute("href");
+// val = link.setAttribute("href", "http://google.com");
+// val = link.hasAttribute("href");
+// link.setAttribute("title", "google");
+// link.removeAttribute("title");
+// val = link;
+
+// console.log(val);
+
+/***
+ * Event Listeners and the event object
+ */
+
+// document.querySelector(".clear-tasks").addEventListener("click", (e) => {
+//   console.log("Hello World");
+//   e.preventDefault();
+// });
+
+// document.querySelector(".clear-tasks").addEventListener("click", onClick);
+
+// function onClick(e) {
+//   console.log("Clicked");
+//   e.preventDefault();
+//   val = e;
+
+//   // Event target element
+//   val = e.target;
+//   val = e.target.id;
+//   val = e.target.className;
+//   val = e.target.classList;
+//   // e.target.innerText = "Hello";
+
+//   // Event type
+//   val = e.type;
+
+//   // Time stamp
+//   val = e.timeStamp;
+
+//   // Coords event relative to the window
+//   val = e.clientY;
+//   val = e.clientX;
+
+//   // Coord event relative to the element
+//   val = e.offsetY;
+//   console.log(val);
+// }
