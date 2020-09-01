64fa7d967a9839a453d956046c6f18aad9d2d6b8
jaywomack <womackjw@hotmail.com>
2020-09-01 11:30:36 -0700

    Finished DOM and fundamentals and moving onto working with DOM projects

--- modern-javascript-from-the-beginning/section-3-DOM-Manipulation/app.js
+++ modern-javascript-from-the-beginning/section-3-DOM-Manipulation/app.js
@@ -426,10 +426,50 @@
 
 // delItem.addEventListener("click", deleteItem);
 
-document.body.addEventListener("click", deleteItem);
+// document.body.addEventListener("click", deleteItem);
 
-function deleteItem(e) {
-  if (e.target.parentElement.classList.contains("delete-item")) {
-    e.target.parentElement.parentElement.remove();
+// function deleteItem(e) {
+//   if (e.target.parentElement.classList.contains("delete-item")) {
+//     e.target.parentElement.parentElement.remove();
+//   }
+// }
+
+// Set local storage item
+// localStorage.setItem("name", "John");
+// localStorage.setItem("age", "30");
+
+// // set session storage item
+// sessionStorage.setItem("name", "Beth");
+
+// get from storage
+// const name = localStorage.getItem("name");
+// const age = localStorage.getItem("age");
+
+// localStorage.clear();
+
+// console.log(name, age);
+
+document.querySelector("form").addEventListener("submit", function (e) {
+  const task = document.getElementById("task").value;
+
+  let tasks;
+
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItems("tasks"));
   }
-}
+
+  tasks.push(task);
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  // alert("Task saved");
+  e.preventDefault();
+});
+
+const tasks = JSON.parse(localStorage.getItems("tasks"));
+
+tasks.forEach(function (task) {
+  console.log(task);
+});
